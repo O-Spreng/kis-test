61f6340a20347a71566b5b003373089307c4038a
O-Spreng <otto.spreng@sou.unifeob.edu.br>
2023-11-07 12:44:35 -0300

    feat: checking if current character is vowel or consonant
    checking if current character is a vowel or a consonant and assigning it according to prefix or stem

--- pig-latin-translator.js
+++ pig-latin-translator.js
@@ -1,9 +1,12 @@
 
 function translate(input) {
   const VALID_LETTER_REGEXP = /[a-z]/i; //Match single caracter in the range between a to z and is case-insensitive
-  const VOWELS = ['a','e','i','o','u'];
+  const VOWELS = ['a','e','i','o','u','y'];
 
   let result = "";
+  let prefix = "";
+  let stem = "";
+  let reachedFirstVowel = false;
 
   // TODO: run through word and stop at first vowel
 
@@ -12,8 +15,23 @@ function translate(input) {
 
     // check if character is a valid letter - working well
     if (input[i].match(VALID_LETTER_REGEXP)) {
-      console.log('Valid letter');
-      result = result.concat(input[i]);
+
+      // check if current character is not a vowel
+      if (!VOWELS.includes(input[i]) && reachedFirstVowel === false) {
+        // if consonant and not reached the first vowel, add it to the prefix
+        console.log("é uma consoante, e ainda não alcançou a primeira vogal");
+        prefix = prefix.concat(input[i]);
+      } else if (reachedFirstVowel === false) {
+        // if vowel and not reached the first vowel, this means it is the first vowel, adding it to the stem and changing flat to true
+        console.log("é uma vogal, e ainda não alcançou a primeira vogal");
+        reachedFirstVowel = true
+        stem = stem.concat(input[i]);
+      } else {
+        // whatever valid letter after reaching the first vowel is added to the stem
+        console.log("é uma vogal, ou uma consoante, e já alcançou a primeira vogal")
+        stem = stem.concat(input[i]);
+      }
+
     } else { // otherwise push anything else into result. - working well
       result = result.concat(input[i]);
     }
@@ -27,8 +45,11 @@ function translate(input) {
 
   }
 
+  //temp log stem + prefix
+  console.log(result);
+
   console.log(result);
   return result;
 }
 
-translate(', No .');
+translate('brasil');
