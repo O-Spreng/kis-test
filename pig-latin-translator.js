8531513b77dd41433f4da1798d7254c18c6cb006
O-Spreng <otto.spreng@sou.unifeob.edu.br>
2023-11-07 12:08:46 -0300

    feat: checking if character is a valid letter
    checking if current character is a valid letter using simple regex
    adding .gitignore file

--- pig-latin-translator.js
+++ pig-latin-translator.js
@@ -1,24 +1,22 @@
 
 function translate(input) {
+  const VALID_LETTER_REGEXP = /[a-z]/i; //Match single caracter in the range between a to z and is case-insensitive
+  const VOWELS = ['a','e','i','o','u'];
+
   let result = "";
 
-  const vowels = ['a','e','i','o','u'];
-
-  // TODO: run through the input string
-  // TODO: identify word
-  // TODO: match if word is capitalized
   // TODO: run through word and stop at first vowel
 
-  const wordRegex = /\w+/g;
-  let wordArray = input.split
-
-
+  // iterate through entire string
   for (let i = 0; i < input.length; i++) {
-    // ??? define stop points such as ' ', ',', '.' '!'
 
-
-
-    vowels.includes(input[i]);
+    // check if character is a valid letter - working well
+    if (input[i].match(VALID_LETTER_REGEXP)) {
+      console.log('Valid letter');
+      result = result.concat(input[i]);
+    } else { // otherwise push anything else into result. - working well
+      result = result.concat(input[i]);
+    }
 
     // run string, push character if not word,
     // find word, set it to current word, pop it from the input str
@@ -27,13 +25,10 @@ function translate(input) {
     // run word, stop at first vowel
     // push anything before first vowel to end + ay
 
-
-
-
   }
 
-
-
+  console.log(result);
+  return result;
 }
 
-translate('Stop');
+translate(', No .');
