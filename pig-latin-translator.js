e218797e87c9849a55b3f172f06faf6ab9fa4cd3
O-Spreng <otto.spreng@sou.unifeob.edu.br>
2023-11-07 14:24:15 -0300

    feat: adding capital letter when needed
    following as 3rd rule states, if a word has a Capital Letter, the translated now is capitalized.

--- pig-latin-translator.js
+++ pig-latin-translator.js
@@ -8,48 +8,66 @@ function translate(input) {
   let stem = "";
   let reachedFirstVowel = false;
 
-  // TODO: run through word and stop at first vowel
-
   // iterate through entire string
   for (let i = 0; i < input.length; i++) {
 
-    // check if character is a valid letter - working well
+    // check if character is a valid letter
     if (input[i].match(VALID_LETTER_REGEXP)) {
 
       // check if current character is not a vowel
-      if (!VOWELS.includes(input[i]) && reachedFirstVowel === false) {
+      if (!VOWELS.includes(input[i].toLowerCase()) && reachedFirstVowel === false) {
         // if consonant and not reached the first vowel, add it to the prefix
-        console.log("é uma consoante, e ainda não alcançou a primeira vogal");
         prefix = prefix.concat(input[i]);
       } else if (reachedFirstVowel === false) {
         // if vowel and not reached the first vowel, this means it is the first vowel, adding it to the stem and changing flat to true
-        console.log("é uma vogal, e ainda não alcançou a primeira vogal");
         reachedFirstVowel = true
         stem = stem.concat(input[i]);
       } else {
         // whatever valid letter after reaching the first vowel is added to the stem
-        console.log("é uma vogal, ou uma consoante, e já alcançou a primeira vogal")
         stem = stem.concat(input[i]);
       }
 
-    } else { // otherwise push anything else into result. - working well
+    } else { // otherwise push anything else into result
+      if (stem !== '') { //checks if stem has any content, if positive, means it has a word before, therefore pushing the word to result first
+        if ((prefix && prefix[0] === prefix[0].toUpperCase()) || ( prefix === '' && stem[0] === stem[0].toUpperCase())) {
+          // checks if prefix first position, or stem first position when prefix empty is uppercase
+          stem = stem[0].toUpperCase().concat(stem.substring(1));
+        }
+        result = result.concat(stem + prefix.toLowerCase() + 'ay');
+        // resetting stem, prefix and
+        stem = "";
+        prefix = "";
+        reachedFirstVowel = false;
+      }
       result = result.concat(input[i]);
     }
 
-    // run string, push character if not word,
-    // find word, set it to current word, pop it from the input str
-
-    // work changes in word, push to result
-    // run word, stop at first vowel
-    // push anything before first vowel to end + ay
+    if (input[i+1] === undefined && input[i].match(VALID_LETTER_REGEXP)) {
+      // checks if prefix first position, or stem first position when prefix empty is uppercase
+      if ((prefix && prefix[0] === prefix[0].toUpperCase()) || ( prefix === '' && stem[0] === stem[0].toUpperCase())) {
+        stem = stem[0].toUpperCase().concat(stem.substring(1));
+      }
+      result = result.concat(stem + prefix.toLowerCase() + 'ay');
+      stem = "";
+      prefix = "";
+      reachedFirstVowel = false;
+    }
 
   }
-
-  //temp log stem + prefix
-  console.log(result);
 
   console.log(result);
   return result;
 }
 
-translate('brasil');
+translate('Stop');
+translate('No littering');
+translate('No shirts, no shoes, no service');
+translate('No persons under 14 admitted');
+translate('Hey buddy, get away from my car!');
+
+translate('This is the story of 1 man, a man named Stanley!');
+translate('IeaAou');
+translate('Fontaine, 14');
+translate('brasil tem farofa, feijao e carne');
+
+
