c3cc4cae8c8f5898c6e17975bbeb0459501f9e0e
O-Spreng <otto.spreng@sou.unifeob.edu.br>
2023-11-07 11:48:12 -0300

    first commit

---
+++ pig-latin-translator.js
@@ -0,0 +1,39 @@
+
+function translate(input) {
+  let result = "";
+
+  const vowels = ['a','e','i','o','u'];
+
+  // TODO: run through the input string
+  // TODO: identify word
+  // TODO: match if word is capitalized
+  // TODO: run through word and stop at first vowel
+
+  const wordRegex = /\w+/g;
+  let wordArray = input.split
+
+
+  for (let i = 0; i < input.length; i++) {
+    // ??? define stop points such as ' ', ',', '.' '!'
+
+
+
+    vowels.includes(input[i]);
+
+    // run string, push character if not word,
+    // find word, set it to current word, pop it from the input str
+
+    // work changes in word, push to result
+    // run word, stop at first vowel
+    // push anything before first vowel to end + ay
+
+
+
+
+  }
+
+
+
+}
+
+translate('Stop');
