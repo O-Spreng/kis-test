f1bbe1614771b810d6832330e035602159a18125
O-Spreng <otto.spreng@sou.unifeob.edu.br>
2023-11-07 14:57:27 -0300

    feat: adding yay for only vowel words
    changing word ending from 'ay' to 'yay' when the word contains no consonants
    adding few examples besides the provided in the sample session
    
    note:
    The following examples were presented in sample session.
    --> No shirts, no shoes, no service
    Onay irtsshay, onay eosshay, onay ervicesay
    
    --> Hey buddy, get away from my car!
    Eyhay uddybah, etgay awayay omfray ymay arcay!
    
    I noticed that "shoes" and "buddy" were misspelled.
    Shoes should be composed of:
    stem: 'oes' + prefix: 'sh' + 'ay'; and it was presented as 'eosshay'
    
    Buddy should be composed of:
    stem: 'uddy' + prefix: 'b' + 'ay'; and it was presented as 'uddybah'

--- pig-latin-translator.js
+++ pig-latin-translator.js
@@ -33,7 +33,12 @@ function translate(input) {
           // checks if prefix first position, or stem first position when prefix empty is uppercase
           stem = stem[0].toUpperCase().concat(stem.substring(1));
         }
-        result = result.concat(stem + prefix.toLowerCase() + 'ay');
+        let stemArr = stem.split('');
+        if (stemArr.every((letter) => VOWELS.includes(letter.toLowerCase()) && prefix === '')) {
+          result = result.concat(stem + 'yay')
+        } else {
+          result = result.concat(stem + prefix.toLowerCase() + 'ay');
+        }
         // resetting stem, prefix and
         stem = "";
         prefix = "";
@@ -47,7 +52,12 @@ function translate(input) {
       if ((prefix && prefix[0] === prefix[0].toUpperCase()) || ( prefix === '' && stem[0] === stem[0].toUpperCase())) {
         stem = stem[0].toUpperCase().concat(stem.substring(1));
       }
-      result = result.concat(stem + prefix.toLowerCase() + 'ay');
+      let stemArr = stem.split('');
+      if (stemArr.every((letter) => VOWELS.includes(letter.toLowerCase()) && prefix === '')) {
+        result = result.concat(stem + 'yay')
+      } else {
+        result = result.concat(stem + prefix.toLowerCase() + 'ay');
+      }
       stem = "";
       prefix = "";
       reachedFirstVowel = false;
@@ -55,19 +65,28 @@ function translate(input) {
 
   }
 
-  console.log(result);
   return result;
 }
 
-translate('Stop');
-translate('No littering');
-translate('No shirts, no shoes, no service');
-translate('No persons under 14 admitted');
-translate('Hey buddy, get away from my car!');
-
-translate('This is the story of 1 man, a man named Stanley!');
-translate('IeaAou');
-translate('Fontaine, 14');
-translate('brasil tem farofa, feijao e carne');
+console.log('\nresult:   '+ translate('Stop'));
+console.log('expected: Opstay');
+console.log('\nresult:   '+ translate('No littering'));
+console.log('expected: Onay itteringlay')
+console.log('\nresult:   '+ translate('No shirts, no shoes, no service'));
+console.log('expected: Onay irtsshay, onay oesshay, onay ervicesay')
+console.log('\nresult:   '+ translate('No persons under 14 admitted'));
+console.log('expected: Onay ersonspay underay 14 admitteday')
+console.log('\nresult:   '+ translate('Hey buddy, get away from my car!'));
+console.log('expected: Eyhay uddybay, etgay awayay omfray ymay arcay!')
+console.log('\nresult:   '+ translate('This is the story of 1 man, a man named Stanley!'));
+console.log('expected: Isthay isay ethay orystay ofay 1 anmay, ayay anmay amednay Anleystay!')
+console.log('\nresult:   '+ translate('IeaAou'));
+console.log('expected: IeaAouyay')
+console.log('\nresult:   '+ translate('Fontaine street, 14'));
+console.log('expected: Ontainefay eetstray, 14')
+console.log('\nresult:   '+ translate('I am a developer'));
+console.log('expected: Iyay amay ayay eveloperday')
+console.log('\nresult:   '+ translate('I am not a powerlifter'));
+console.log('expected: Iyay amay otnay ayay owerlifterpay')
 
 
